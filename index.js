4793db03825df4d042c37092af5f039261b8d195
didof <didonato.fr@gmail.com>
2021-11-13 18:56:06 +0100

    graphic fix

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
 const path = require('path')
-
 const express = require('express')
 
 const app = express()
+app.enable('etag');
 
 const KEY = 'user'
-
 const zombies = new Map()
 
 app.get('/', (_, res) => {
   res.sendFile(path.join(__dirname, 'assets', 'index.html'))
 })
 
-app.get('/zombie', (req, res) => {
-  const etag = req.headers['if-none-match']
-
-  if (req.headers.cookie) {
-    const user = req.headers.cookie.split('=')[1]
-    zombies.set(etag, user)
-  } else {
-    const user = zombies.get(etag)
-    if (user) {
-      res.setHeader('set-cookie', [`${KEY}=${user}`])
-    }
-  }
-
-  res.sendFile(path.join(__dirname, 'assets', 'zombie.html'))
-})
-
 app.get('/login', (req, res) => {
   const user = req.query.user
   res.setHeader('set-cookie', [`${KEY}=${user}`])
-  res.status(201).send(`user ${user} has been created.`)
-})
 
-app.get('/zombielogin', (req, res) => {
-  const user = req.query.user
-  res.setHeader('set-cookie', [`${KEY}=${user}`])
-
-  const etag = req.headers['if-none-match']
-  zombies.set(etag, user)
-  
   res.status(201).send(`user ${user} has been created.`)
 })
 
 app.get('/logout', (_, res) => {
   res.clearCookie(KEY)
   res.sendStatus(200)
+})
+
+app.get('/zombie', (req, res) => {
+  console.log('[ROUTE] zombie')
+
+  const etag = req.headers['if-none-match']
+
+  if (req.headers.cookie) {
+    // take for granted only `user` cookie exists
+
+    const user = req.headers.cookie.split('=')[1]
+    zombies.set(etag, user)
+
+    console.info(`/zombie, New zombie: [${etag}:${user}]`)
+  } else {
+    const user = zombies.get(etag)
+    if (user) res.setHeader('set-cookie', [`${KEY}=${user}`])
+  }
+
+  res.sendFile(path.join(__dirname, 'assets', 'zombie.html'))
+})
+
+app.get('/zombielogin', (req, res) => {
+  console.log('[ROUTE] zombielogin')
+
+  const user = req.query.user
+  res.setHeader('set-cookie', [`${KEY}=${user}`])
+
+  const etag = req.headers['if-none-match']
+  zombies.set(etag, user)
+
+  res.status(201).send(`user ${user} has been created.`)
 })
 
 const PORT = process.env.PORT || 3000
